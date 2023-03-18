79eecc28b9588734cd72275058f1e352acf2de90
LakGillJPN <lakgill88@gmail.com>
2023-03-18 15:28:47 +0900

    feat: initialize and configure express server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const setupServer = require('./server');
+const PORT = process.env.PORT || 4000; // process.env will assign you a port
+const server = setupServer();
+
+( async () => {
+  try {
+    server.listen(PORT, () => {
+      console.log(`app is listening at http://localhost:${PORT}`)
+    })
+
+  } catch (err) {
+    console.error(`app failed to start ${err}`)
+
+  }
+})();
