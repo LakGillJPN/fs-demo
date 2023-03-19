6dafa35530408992e7be88626cd77217d094b970
LakGillJPN <lakgill88@gmail.com>
2023-03-20 02:04:09 +0900

    set up a basic full stack application - commit for server files

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+//const cors = require('cors')
 const db = require('../db/knex')
 
 function setupServer() {
   const app = express();
 
   app.use(express.json());
+  //app.use(cors())
 
   app.get('/hello', (req, res) => {
     res.send('world');
