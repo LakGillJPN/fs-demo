0100251ae5e2cb285eea17f4c39e2f8836585639
LakGillJPN <lakgill88@gmail.com>
2023-03-28 17:12:31 +0900

    fix: fix build script

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const db = require('../db/knex');
 
 ( async () => {
   try {
-    await db.migrate.latest(); 
     server.listen(PORT, () => {
       console.log(`app is listening at http://localhost:${PORT}`)
     })
